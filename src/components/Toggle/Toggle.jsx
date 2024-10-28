a6e7e820022a144241700860af7dcf5c6f5fb680
Abdulla Al Muhit <muhitabdullah279@gmail.com>
2024-10-28 06:23:15 +0600

    Scroll to the top of the components feature has added to toggle buttons.

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -47,14 +47,22 @@ const Toggle = ({selectedPlayers, handlePurchase, handleRemove}) => {
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
+    const scrollToComponent = () => {
+        const element = document.getElementById("targetComponent");
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
     return (
         <div className="max-w-[95%] sm:max-w-[90%] lg:max-w-[81.25%] min-[1920px]:max-w-[97.5rem] mx-auto">
             {/* Toggle section */}
+            <div id="targetComponent"></div>
             <div className="w-full flex flex-col sm:flex-row items-center gap-4 sm:gap-0 justify-center sm:justify-between mb-8 sticky z-10 top-5 sm:top-[5.7rem] lg:top-[6.9rem]">
                 <h2 className={`text-xl lg:text-2xl font-bold text-center sm:text-left ${isScrolled ? "invisible" : "visible"}`}>{isActive ? 'Available Players' : `Selected Player (${selectedPlayers.length}/6)`}</h2>
                 <div className="flex items-center border border-[#1313131A] rounded-xl">
-                    <div onClick={() => handleIsActive('available')} className={`w-[45%] px-6 py-3 text-xs sm:text-sm lg:text-base border-r border-[#1313131A] rounded-l-xl ${isActive ? 'bg-[#E7FE29] text-black font-bold' : 'bg-white text-[#13131399] font-normal'}`}>Available</div>
-                    <div onClick={() => handleIsActive('')} className={`w-[55%] px-6 py-3 text-xs sm:text-sm lg:text-base rounded-r-xl ${!isActive ? 'bg-[#E7FE29] text-black font-bold' : 'bg-white text-[#13131399] font-normal'}`}>Selected ({selectedPlayers.length})</div>
+                    <div onClick={() => {handleIsActive('available'); scrollToComponent();}} className={`w-[45%] px-6 py-3 text-xs sm:text-sm lg:text-base border-r border-[#1313131A] rounded-l-xl ${isActive ? 'bg-[#E7FE29] text-black font-bold' : 'bg-white text-[#13131399] font-normal'}`}>Available</div>
+                    <div onClick={() => {handleIsActive(''); scrollToComponent()}} className={`w-[55%] px-6 py-3 text-xs sm:text-sm lg:text-base rounded-r-xl ${!isActive ? 'bg-[#E7FE29] text-black font-bold' : 'bg-white text-[#13131399] font-normal'}`}>Selected ({selectedPlayers.length})</div>
                 </div>    
             </div>
 
